5541e94b4949663462130f2d9ceaef8c9f949b68
Andrii Reblian <andrii.reblian@gmail.com>
2021-08-17 12:28:17 +0300

    hometask3

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -8,6 +8,9 @@ function MovieItem({img, title, year, genre}) {
                 <div className="movie-list__thumbnail">
                     <img src={img} alt="Image"/>
                 </div>
+                <div className="movie-list__dots hide">
+                    <div className="movie-list__dots-item"></div>
+                </div>
                 <div className="movie-list__description">
                     <span className="movie-list__description-title">{title}</span>
                     <span className="movie-list__description-year">{year}</span>
