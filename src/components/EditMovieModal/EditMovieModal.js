c3cc71f898f91978f5fc4171266b0dde24b4a188
Andrii Reblian <andrii.reblian@gmail.com>
2021-08-30 22:02:55 +0300

    hometask 4

--- src/components/EditMovieModal/EditMovieModal.js
+++ src/components/EditMovieModal/EditMovieModal.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import {Modal, Button, Form, Row, Col} from "react-bootstrap";
-import './AddMovie.scss';
+import React, {useState} from "react";
+import {Modal, Button, Col, Form, Row} from "react-bootstrap";
 
-const AddMovieForm = ({ onSubmit }) => {
+const EditMovieForm = ({ onSubmit }) => {
+    const [id, setID] = useState("");
     const [title, setTitle] = useState("");
     const [date, setReleaseDate] = useState("");
     const [url, setUrl] = useState("");
@@ -12,7 +12,17 @@ const AddMovieForm = ({ onSubmit }) => {
 
     return (
         <Form onSubmit={onSubmit}>
-            <Form.Group controlId="formAddMovieTitle">
+            <Form.Group controlId="formEditMovieID">
+                <Form.Label>Movie ID</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="MO32820TH"
+                    value={id}
+                    onChange={(e) => setID(e.target.value)}
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formEditMovieTitle">
                 <Form.Label>Title</Form.Label>
                 <Form.Control
                     type="text"
@@ -22,7 +32,7 @@ const AddMovieForm = ({ onSubmit }) => {
                 />
             </Form.Group>
 
-            <Form.Group controlId="formAddMovieReleaseDate">
+            <Form.Group controlId="formEditMovieReleaseDate">
                 <Form.Label>Release Date</Form.Label>
                 <Form.Control
                     type="date"
@@ -31,17 +41,17 @@ const AddMovieForm = ({ onSubmit }) => {
                 />
             </Form.Group>
 
-            <Form.Group controlId="formAddMovieUrl">
+            <Form.Group controlId="formEditMovieUrl">
                 <Form.Label>Movie URL</Form.Label>
                 <Form.Control
                     type="text"
-                    placeholder="Movie here"
+                    placeholder="www.moana.com"
                     value={url}
                     onChange={(e) => setUrl(e.target.value)}
                 />
             </Form.Group>
 
-            <Form.Group controlId="formAddMovieGenre">
+            <Form.Group controlId="formEditMovieGenre">
                 <Form.Label>Genre</Form.Label>
                 <Form.Select defaultValue="Choose..." onChange={(e) => setGenre(e.target.value)}>>
                     <option value="1">One</option>
@@ -50,21 +60,21 @@ const AddMovieForm = ({ onSubmit }) => {
                 </Form.Select>
             </Form.Group>
 
-            <Form.Group controlId="formAddMovieOverview">
+            <Form.Group controlId="formEditMovieOverview">
                 <Form.Label>Overview</Form.Label>
                 <Form.Control
                     type="text"
-                    placeholder="Overview here"
+                    placeholder="Overview text goes here"
                     value={overview}
                     onChange={(e) => setOverview(e.target.value)}
                 />
             </Form.Group>
 
-            <Form.Group controlId="formAddMovieRuntime">
+            <Form.Group controlId="formEditMovieRuntime">
                 <Form.Label>Runtime</Form.Label>
                 <Form.Control
                     type="text"
-                    placeholder="Runtime here"
+                    placeholder="Runtime text goes here"
                     value={runtime}
                     onChange={(e) => setRuntime(e.target.value)}
                 />
@@ -75,15 +85,15 @@ const AddMovieForm = ({ onSubmit }) => {
                     Reset
                 </Button>
                 <Button variant="danger" type="submit">
-                    Submit
+                    Save
                 </Button>
             </Form.Group>
         </Form>
     );
 };
 
-function AddMovieModal(props) {
-    const onAddFormSubmit = (e) => {
+function EditMovieModal(props) {
+    const onEditFormSubmit = (e) => {
         e.preventDefault();
         handleClose();
     };
@@ -96,30 +106,14 @@ function AddMovieModal(props) {
         >
             <Modal.Header closeButton closeVariant='white'>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    Add movie
+                    Edit Movie
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <AddMovieForm onSubmit={onAddFormSubmit} />
+                <EditMovieForm onSubmit={onEditFormSubmit} />
             </Modal.Body>
         </Modal>
     );
 }
 
-function AddMovie() {
-    const [modalShow, setModalShow] = React.useState(false);
-
-    return (
-        <>
-            <div className="btn-container">
-                <Button variant="secondary" variant="primary" onClick={() => setModalShow(true)}>+ Add movie</Button>{' '}
-            </div>
-
-            <AddMovieModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-        </>
-    );
-}
-export default AddMovie;
+export default EditMovieModal;
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,27 +1,53 @@
 import React from "react";
 import { PropTypes } from 'prop-types';
+import {Dropdown, CloseButton, Button} from "react-bootstrap";
 import './MovieItem.scss';
+import EditMovieModal from "../EditMovieModal/EditMovieModal";
+import DeleteMovieModal from "../DeleteMovieModal/DeleteMovieModal";
+
 
 function MovieItem(props) {
+    const [EditModalShow, setEditModalShow] = React.useState(false);
+    const [DeleteModalShow, setDeleteModalShow] = React.useState(false);
+
     return (
-        <li className="movie-list__item">
-            <div className="movie-list__thumbnail">
-                <img src={props.img} alt="Image"/>
-            </div>
-            <div className="movie-list__dots hide">
-                <div className="movie-list__dots-item"></div>
-            </div>
-            <div className="movie-list__description">
-                <span className="movie-list__description-title">{props.title}</span>
-                <span className="movie-list__description-year">{props.year}</span>
-            </div>
-            <p className="movie-list__genre">{props.genre}</p>
-        </li>
+        <>
+            <li className="movie-list__item">
+                <div className="movie-list__thumbnail">
+                    <img src={props.img} alt="Image"/>
+                </div>
+                <Dropdown className="movie-list__dots hide" autoClose="inside">
+                    <Dropdown.Toggle variant="" id="dropdown-basic" className="movie-list__dots-item"><span>...</span>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className="movie-list__dropdown-container">
+                        <Dropdown.Item href="#" className="close-btn"><CloseButton variant="white"/></Dropdown.Item>
+                        <Dropdown.Item href="#/action-2" onClick={() => setEditModalShow(true)}>Edit</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3" onClick={() => setDeleteModalShow(true)}>Delete</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <div className="movie-list__description">
+                    <span className="movie-list__description-title">{props.title}</span>
+                    <span className="movie-list__description-year">{props.year}</span>
+                </div>
+                <p className="movie-list__genre">{props.genre}</p>
+            </li>
+
+            <EditMovieModal
+                show={EditModalShow}
+                onHide={() => setEditModalShow(false)}
+            />
+
+            <DeleteMovieModal
+                show={DeleteModalShow}
+                onHide={() => setDeleteModalShow(false)}
+            />
+        </>
     );
 }
 
 MovieItem.propTypes = {
-    img: PropTypes.string,
+    img: PropTypes.string.isRequired,
     title: PropTypes.string,
     year: PropTypes.number,
     genre: PropTypes.string
